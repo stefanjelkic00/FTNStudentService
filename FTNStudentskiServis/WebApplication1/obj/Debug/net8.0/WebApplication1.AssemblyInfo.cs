198148bdbfd5718c0bae8b698eedd824f6ae9e7b
stefanjelkic00 <stefanjelkic2@gmail.com>
2025-03-28 19:15:12 +0100

    Popravljeni API pozivi i prikaz podataka na frontendu

--- FTNStudentskiServis/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ FTNStudentskiServis/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64851b79ff0c779e6e89b170cd88387c26bc3a55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caae3cc77e60ea1ae60e8369112acaf3459fd688")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
